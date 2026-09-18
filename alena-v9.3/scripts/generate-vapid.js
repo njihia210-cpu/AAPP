10d6586084f14c4a6a44bfdcea92961cb4fb4ff4
njihia210-cpu <njihia210@gmail.com>
2026-09-18 11:35:42 +0200

    Delete alena-v9.3 directory

--- alena-v9.3/scripts/generate-vapid.js
+++
@@ -1,9 +0,0 @@
-/* Erzeugt ein VAPID-Schlüsselpaar für Web Push.
-   Aufruf:  node scripts/generate-vapid.js                              */
-import webpush from 'web-push';
-
-const keys = webpush.generateVAPIDKeys();
-console.log('\nDiese beiden Werte in Vercel als Umgebungsvariablen eintragen:\n');
-console.log('VAPID_PUBLIC_KEY =', keys.publicKey);
-console.log('VAPID_PRIVATE_KEY =', keys.privateKey);
-console.log('\nDen privaten Schlüssel niemals ins Repository legen.\n');
